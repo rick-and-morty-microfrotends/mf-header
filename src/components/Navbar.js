d12f6ee66a8dc745497ab9280560f20255dfa2f6
Lion <breakermoob@hotmail.com>
2022-05-09 19:03:51 -0500

    Initial Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
+            <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Left</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="mx-auto my-auto order-0">
+                <img width="120px" src="https://logos-marcas.com/wp-content/uploads/2022/01/Rick-And-Morty-Logo.png" alt="Logo" class="rounded-circle" />
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Right</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
